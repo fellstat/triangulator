5865a7e6441ffecbd8767c8a2003486e64bb7a29
ifellows <ianfellows@athena.attlocal.net>
2022-12-28 15:41:26 -0800

    make R CMD CHECK happy

--- R/triangulator-package.R
+++ R/triangulator-package.R
@@ -9,8 +9,23 @@
 #' @import methods
 #' @import Rcpp
 #' @importFrom rstan sampling
+#' @importFrom stats var
+#' @importFrom RcppParallel CxxFlags
+#' @importFrom ggplot2 ggplot
+#' @importFrom patchwork area
+#' @importFrom rhandsontable hot_col
+#' @importFrom rstantools init_cpp
+#' @importFrom shinyhelper helper
 #'
 #'
 NULL
 
 # roxygen2::roxygenise()
+
+# Silence R CMD CHECK because it doesn't see the shiny app
+RcppParallel::CxxFlags
+ggplot2::ggplot
+patchwork::area
+rhandsontable::hot_col
+rstantools::init_cpp
+shinyhelper::helper
